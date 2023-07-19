33ebac04c17e87d24bc60b2c2685e6135c3c3f13
casvanluijtelaar <casvanluijtelaar@hotmail.com.com>
2023-07-19 18:36:25 +0200

    fix regex

--- utils/modparser.js
+++ utils/modparser.js
@@ -4,8 +4,8 @@
 export const parseMods = (html) => {
     const mods = Array.from(html.querySelectorAll("td[data-type='DisplayName']"));
 
-    const allowedCharacterRegex = new RegExp("[^a-zA-Z0-9_'\\[\\] -]+");
-    const optimizeSpacesRegex = new RegExp("[ \t]+");
+    const allowedCharacterRegex = /[^a-zA-Z0-9_'\[\] -]+/g;
+    const optimizeSpacesRegex = /[ \t]+/;
 
     const parsedMods = mods.map(mod => mod.innerText.replace(allowedCharacterRegex, "").replace(optimizeSpacesRegex, " "));
     return parsedMods.map(mod => `@${mod};`).join("")
