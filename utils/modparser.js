f1eabdb155bc47aaaa9a53a5eb55d81302a228fb
Cas van Luijtelaar <67225314+casvanluijtelaar@users.noreply.github.com>
2026-03-26 21:45:44 +0100

    fix parsing

--- utils/modparser.js
+++ utils/modparser.js
@@ -5,7 +5,7 @@ export const parseMods = (html, allowedCharacters) => {
     const mods = Array.from(html.querySelectorAll("td[data-type='DisplayName']"));
 
     const escapedAllowedCharacters = allowedCharacters.split("").map(char => "\\" + char).join("")
-    const allowedCharacterRegex = new RegExp(`[^a-zA-Z0-9${escapedAllowedCharacters}]+`);
+    const allowedCharacterRegex = new RegExp(`[^a-zA-Z0-9${escapedAllowedCharacters}]+`, "g");
     const optimizeSpacesRegex = /[ \t]+/;
 
     const parsedMods = mods.map(mod => mod.innerText.replace(allowedCharacterRegex, "").replace(optimizeSpacesRegex, " "));
